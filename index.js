b537a328570ade3b566c3f406937c015393b3784
Balwant <bbirajdar7@gmail.com>
2020-06-06 23:50:20 +0530

    javascript files added

---
+++ index.js
@@ -0,0 +1,22 @@
+const searchForm = document.querySelector('.search-location');
+const cityValue = document.querySelector('.search-location input');
+const cityName = document.querySelector('.city-name p')
+
+
+
+updateWeatherApp = (city) => {
+    cityName.textContent = city.name;
+}
+
+//add an event listner to the form
+searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const citySearched = cityValue.value.trim();
+    console.log(citySearched);
+    searchForm.reset();
+
+    requestCity(citySearched)
+        .then((data) => { updateWeatherApp(data) })
+        .catch((error) => { console.log(error) })
+
+})
